a7362b4ec4d5fbbc08e6f3615d6666073900894e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-17 14:50:30 -0700

    - Created a (fake) DAO for quotes (going to use a fake database)

---
+++ app/src/main/java/com/nfragiskatos/quoteapp_mvvmbasics/data/FakeQuoteDao.kt
@@ -0,0 +1,23 @@
+package com.nfragiskatos.quoteapp_mvvmbasics.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * Created by Nicholas Fragiskatos on 4/17/2020.
+ */
+class FakeQuoteDao {
+    private val quoteList = mutableListOf<Quote>()
+    private val quotes = MutableLiveData<List<Quote>>()
+
+    init {
+        quotes.value = quoteList
+    }
+
+    fun addQuote(quote: Quote) {
+        quoteList.add(quote)
+        quotes.value = quoteList
+    }
+
+    fun getQuotes() = quotes as LiveData<List<Quote>>
+}
