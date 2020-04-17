76ea338b2f129921f8db49ff909424ea3ab26466
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-17 15:32:52 -0700

    - Modified quotes activity to initialize the ui 1. "Injected" QuotesViewModelFactory via the InjectorUtils 2. Got a QuotesViewModel w/ help from the factory 3. Observed the live data (quotes list) and updated the quotes text view with the latest list of quotes when changed. 4. Set a click listener on the add quote button to create a new quote and pass it to the ViewModel, which will in turn pass it to the repository for persistence

--- app/src/main/java/com/nfragiskatos/quoteapp_mvvmbasics/UI/quotes/QuotesActivity.kt
+++ app/src/main/java/com/nfragiskatos/quoteapp_mvvmbasics/UI/quotes/QuotesActivity.kt
@@ -1,13 +1,39 @@
 package com.nfragiskatos.quoteapp_mvvmbasics.UI.quotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.nfragiskatos.quoteapp_mvvmbasics.R
+import com.nfragiskatos.quoteapp_mvvmbasics.data.Quote
+import com.nfragiskatos.quoteapp_mvvmbasics.utilities.InjectorUtils
+import kotlinx.android.synthetic.main.activity_quotes.*
 
 class QuotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quotes)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        val factory = InjectorUtils.provideQuotesViewModelFactory()
+        val viewModel = ViewModelProviders.of(this, factory)
+            .get(QuotesViewModel::class.java)
+        viewModel.getQuotes().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach { quote ->
+                stringBuilder.append("$quote\n\n")
+            }
+            textView_quotes.text = stringBuilder.toString()
+        })
+
+        button_add_quote.setOnClickListener {
+            val quote = Quote(editText_quote.text.toString(), editText_author.text.toString())
+            viewModel.addQuote(quote)
+            editText_quote.setText("")
+            editText_author.setText("")
+        }
     }
 }
